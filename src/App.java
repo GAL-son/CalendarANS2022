95dd86586de310aed0abf6bac97112880acc19fe
GAL_son <>
2023-01-09 22:29:16 +0100

    ComparatorTest

--- src/App.java
+++ src/App.java
@@ -1,8 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Date d1 = new Date(29, 2, 2000);
+        Date d0 = new Date(2,1,2023);
+        Date d1 = new Date(1, 1, 1000);
+        Date d2 = new Date(19, 3, 666);
 
-        System.out.println(d1 + " IsLeapYear? - " + d1.isLeapYear());
+        ArrayList<Date> list = new ArrayList<>();
+        list.add(d2);
+        list.add(d1);
+        list.add(d0);
+
+        System.out.println(list);
+
+        Collections.sort(list);
+
+        System.out.println(list);
         
     }
 }
