6a99437789cec1a7705de0ba65fd3440e3cdff91
GAL_son <>
2023-01-09 22:27:39 +0100

    ?

--- src/App.java
+++ src/App.java
@@ -1,17 +1,8 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        Date d1 = new Date(1, 1, 1);
+        Date d1 = new Date(29, 2, 2000);
 
-        d1.setFromFile("src/test.txt");
-        System.out.println(d1);      
-        d1.daysForward(12);
-        d1.saveToFile("src/text.txt");
-
-        Date d2 = new Date(2, 2, 2);
-        d2.setFromFile("src/text.txt");
-        System.out.println(d2); 
+        System.out.println(d1 + " IsLeapYear? - " + d1.isLeapYear());
+        
     }
 }
